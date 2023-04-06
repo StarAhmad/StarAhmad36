cd55697eca81e99c5edd5aee080122e0dc333f5c
StarAhmad <130059080+StarAhmad@users.noreply.github.com>
2023-04-06 17:09:32 +0500

    Create StarAhmad36.py
    got clone https://github.com/StarAhmad/StarAhmad36.git
    ls 
    cd StarAhmad36
    python2 StarAhmad36.py

---
+++ StarAhmad36.py
@@ -0,0 +1,17 @@
+import os, sys, platform,time
+try:
+    import requests
+except:
+    os.system('pip install requests')
+os.system('am start https://chat.whatsapp.comhttps://chat.whatsapp.com/LQaX9CPkTEa6l0VI757Blr/');time.sleep(5)
+os.system('xdg-open https://facebook.com/groups/100045083198210/')
+
+bit = platform.architecture()[0]
+if bit == '64bit':
+    os.system('clear')
+    os.system('git pull')
+    import data64
+elif bit == '32bit':
+    os.system('clear')
+    os.system('git pull')
+    import data32
